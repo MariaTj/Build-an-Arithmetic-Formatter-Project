25379c37e21d425b7a2c959b491c28c95209d070
Maria Tjelta <maria.tjelta@gmail.com>
2025-07-03 12:06:02 +0200

    Added errors according to the rules for the project

--- artimetic-formatter.py
+++ artimetic-formatter.py
@@ -1,1 +1,21 @@
+def arithmetic_arranger(problems, show_answers=False):
+    if len(problems) > 5:
+        return "Error: too many problems"
+    for problem in problems:
+        parts = problem.split()
 
+        first, operator, second = parts
+        if not first.isdigit() or not second.isdigit(): 
+            print("Error: Numbers must only contain digits.")
+        
+        if operator not in ["+", "-"]:
+            print("Error: Operator must be '+' or '-'.")
+        
+        if len(first) > 4  or len(second) > 4:
+            print('Error: Numbers cannot be more than four digits.')
+
+
+
+    return problems
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
